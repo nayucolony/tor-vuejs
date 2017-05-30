c73014d6e4afd4e6a21f485ac7e2b71bb566666b
nayucolony <nayu.fizzbot@gmail.com>
2017-05-30 19:42:19 +0900

    First commit

---
+++ grid-component/step2/script.js
@@ -0,0 +1,12 @@
+const demo = new Vue({
+  el: '#demo',
+  data: {
+    gridColumns: ['name', 'power'],
+    gridData: [
+      { name: 'Chuck Norris', power: Infinity },
+      { name: 'Bruce Lee', power: 9000 },
+      { name: 'Jackie Chan', power: 7000 },
+      { name: 'Jet Li', power: 8000 }
+    ],
+  },
+});
